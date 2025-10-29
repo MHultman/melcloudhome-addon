a77e62abab8219dc8e1463b2fee9db5c0d678c27
Martin Hultman <maartin.hultman@gmail.com>
2025-10-29 18:40:14 +0100

    chore: Bump version to 1.4.1 - Patch release for MQTT logging fix

--- melcloudhome-bridge/app/__init__.py
+++ melcloudhome-bridge/app/__init__.py
@@ -1,5 +1,5 @@
 """MELCloud Home Bridge - Bridge Mitsubishi MELCloud devices to Home Assistant via MQTT."""
 
-__version__ = "1.4.0"
+__version__ = "1.4.1"
 __author__ = "MELCloud Home Bridge"
 __description__ = "Home Assistant add-on that bridges MELCloud climate devices via MQTT Discovery"
